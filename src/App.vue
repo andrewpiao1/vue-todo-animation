94b25647a1f0c2bf0600a41bde088375036fa9d8
Andrew Piao <andrew.piao@gopuff.com>
2020-11-30 00:06:38 -0500

    app initialization; set up scss

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to my Vue.js Todo app"/>
   </div>
 </template>
 
@@ -16,7 +16,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
