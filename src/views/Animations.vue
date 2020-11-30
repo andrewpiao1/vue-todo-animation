fc5a5902a1ed9cccec001f138e22ce9f000c3ff4
Andrew Piao <andrew.piao@gopuff.com>
2020-11-30 08:31:51 -0500

    added working drop-down animation; added title animation

---
+++ src/views/Animations.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="animations">
+    <h1>Animations</h1>
+
+    <div class="box2">
+      <button class="header" @click="clickExpand">{{ expanded }}</button>
+      <div class="content" :class="{ active: expanded }">
+        <ul>
+          <li>line</li>
+          <li>line</li>
+          <li>line</li>
+          <li>line</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Animations',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      expanded: false
+    }
+  },
+  methods: {
+    clickExpand() {
+      this.expanded = !this.expanded
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.box2 {
+  display: flex;
+  flex-direction: column;
+  width: 30em;
+  margin: auto;
+
+  .header {
+    height: 2em;
+    background: lightcoral;
+    transform: scaleY(1);
+
+    animation-name: expand;
+    animation-duration: .5s;
+  }
+  .content {
+    background: lightcyan;
+    transform-origin: top;
+    transform: scaleY(0);
+    transition: transform 0.1s ease;
+
+    ul {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+    }
+  }
+
+  .active {
+    transform: scaleY(1);
+  }
+
+  @keyframes expand {
+    from {
+      transform-origin: top;
+      transform: scaleY(0)
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <div class=image-box>
+      <img alt="Vue logo" src="../assets/logo.png">
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+.image-box{
+  background: lightblue;
+  height: 8em;
+  margin-bottom: 5em;
+
+    img{
+      transform: scaleY(1);
+      animation: imgExpand 1s;
+    }
+
+    @keyframes imgExpand {
+      from{transform:scaleY(0)}
+    }
+}
+</style>
